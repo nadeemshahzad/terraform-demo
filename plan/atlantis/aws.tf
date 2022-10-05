29928cf64564f4b1337a20d5d6c4a305a626ed1e
nadeemshahzad <mshahzad@2u.com>
2022-10-05 11:24:22 +0500

    test

--- plan/atlantis/aws.tf
+++ plan/atlantis/aws.tf
@@ -6,18 +6,3 @@ variable "aws_region" {
 provider "aws" {
   region  = var.aws_region
 }
-
-terraform {
-  required_version = ">= 0.12"
-  required_providers {
-    aws = " 3.23.0"
-  }
-  backend "s3" {
-    encrypt                 = true
-    bucket                  = "terraform-demo-state-0001"
-    key                     = "plan/atlantis"
-    dynamodb_table          = "terraform-dynamodb-state-lock"
-    region                  = "us-west-2"
-    skip_metadata_api_check = true
-  }
-}

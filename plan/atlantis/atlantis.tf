d8f777eec7090ff56db8a9dc0dc4a7504762d166
nadeemshahzad <mshahzad@2u.com>
2022-10-06 11:22:06 +0500

    atlatnis

--- plan/atlantis/atlantis.tf
+++ plan/atlantis/atlantis.tf
@@ -21,3 +21,12 @@ resource "aws_s3_bucket" "b" {
 #    Environment = "Dev"
 #  }
 }
+
+resource "aws_s3_bucket" "c" {
+  bucket = "my-tfffff-test-bucket"
+
+#  tags = {
+#    Name        = "My bucket"
+#    Environment = "Dev"
+#  }
+}

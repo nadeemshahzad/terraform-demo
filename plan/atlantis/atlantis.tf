784aa42642a9e0f233c0aabf4964a916a380f5f3
nadeemshahzad <mshahzad@2u.com>
2022-10-06 11:19:36 +0500

    b

--- plan/atlantis/atlantis.tf
+++ plan/atlantis/atlantis.tf
@@ -12,3 +12,12 @@ module "atlantis_vpc" {
   vpc_name = var.atlantis_vpc_name
   vpc_cidr_block = var.atlantis_cidr_block
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tffff-test-bucket"
+
+#  tags = {
+#    Name        = "My bucket"
+#    Environment = "Dev"
+#  }
+}
